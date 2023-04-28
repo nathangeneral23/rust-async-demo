f6a4ea5951cd7317acdce975a9c9c25d1bab9d38
Nathan General <51286292+nathangeneral23@users.noreply.github.com>
2023-04-29 09:21:39 +0930

    Initialize

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::time::Duration;
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use tokio::task;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    task::spawn(async {
+        loop {
+            tokio::time::sleep(Duration::from_secs(5)).await;
+            println!("5 seconds passed!")
+        }
+    });
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
